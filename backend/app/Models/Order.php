218acc9b794f3ece33fa3772de2cff8d236f0c10
reddcs <chakirm082@gmail.com>
2026-04-13 11:17:59 +0100

    feat(backend-models): add Order model

---
+++ backend/app/Models/Order.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\OrderItem;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Order extends Model
+{
+    protected $fillable = [
+        'client_id',
+        'status',
+        'total_price',
+        'phone',
+        'city',
+        'zip_code',
+        'address',
+    ];
+
+    public function client()
+    {
+        return $this->belongsTo(User::class, 'client_id');
+    }
+
+    public function items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
+}
