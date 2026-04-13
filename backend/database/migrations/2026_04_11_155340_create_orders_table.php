0b6fe7b238c72abec6dd234e38cfad342807c087
reddcs <chakirm082@gmail.com>
2026-04-13 11:18:00 +0100

    feat(backend-migrations): add orders table migration

---
+++ backend/database/migrations/2026_04_11_155340_create_orders_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('client_id')->constrained('users')->cascadeOnDelete();
+            $table->enum('status',['pending','confirmed','delivery','rejected'])->default('pending');
+            $table->decimal('total_price',10,2);
+            $table->string('phone');
+            $table->string('city');
+            $table->string('zip_code');
+            $table->text('address');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
