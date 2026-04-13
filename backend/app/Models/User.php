31bf41fa8f8cde91d8d7d238d3436803347d9e96
reddcs <chakirm082@gmail.com>
2026-04-13 11:17:59 +0100

    feat(backend-models): add User model

---
+++ backend/app/Models/User.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Attributes\Hidden;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+use App\Models\Store;
+use App\Models\Order;
+#[Fillable(['name', 'email', 'password','role','is_banned'])]
+#[Hidden(['password', 'remember_token'])]
+class User extends Authenticatable
+{
+    /** @use HasFactory<UserFactory> */
+    use HasApiTokens, HasFactory, Notifiable;
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'email_verified_at' => 'datetime',
+            'password' => 'hashed',
+        ];
+    }
+
+    public function storeProfile(){
+        return $this->hasOne(Store::class, 'user_id');
+    }
+
+    public function orders(){
+        return $this->hasMany(Order::class,'client_id');
+    }
+}
