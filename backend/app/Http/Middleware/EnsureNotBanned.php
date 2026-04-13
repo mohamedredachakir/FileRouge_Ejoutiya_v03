b59706aa60b7c4a5db140417883aa18f7a08823e
reddcs <chakirm082@gmail.com>
2026-04-13 11:17:59 +0100

    feat(backend-middleware): add EnsureNotBanned middleware

---
+++ backend/app/Http/Middleware/EnsureNotBanned.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class EnsureNotBanned
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        $user = $request->user();
+
+        if ($user && $user->is_banned) {
+            return response()->json(['message' => 'Your account is banned.'], 403);
+        }
+
+        return $next($request);
+    }
+}
