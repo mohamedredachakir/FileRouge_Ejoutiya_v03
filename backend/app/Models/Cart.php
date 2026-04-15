f675aaa8f3912ecba7b023b91bdb8ae73dc539ea
reddcs <chakirm082@gmail.com>
2026-04-15 15:44:31 +0100

    model(cart): add cart model and relations

---
+++ backend/app/Models/Cart.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use App\Models\CartItem;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Cart extends Model
+{
+    protected $fillable = [
+        'client_id',
+    ];
+
+    public function client(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'client_id');
+    }
+
+    public function items(): HasMany
+    {
+        return $this->hasMany(CartItem::class);
+    }
+}
