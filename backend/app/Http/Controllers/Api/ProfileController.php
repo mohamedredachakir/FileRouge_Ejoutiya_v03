387685a0c15ce91f47e719a2404ea6c2097764f0
reddcs <chakirm082@gmail.com>
2026-04-15 15:44:36 +0100

    controller(profile): switch update to form request

--- backend/app/Http/Controllers/Api/ProfileController.php
+++ backend/app/Http/Controllers/Api/ProfileController.php
@@ -3,8 +3,8 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Profile\UpdateProfileRequest;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 
 class ProfileController extends Controller
 {
@@ -16,15 +16,11 @@ class ProfileController extends Controller
         ]);
     }
 
-    public function update(Request $request)
+    public function update(UpdateProfileRequest $request)
     {
         $user = $request->user();
 
-        $validated = $request->validate([
-            'name' => ['sometimes', 'string', 'max:255'],
-            'email' => ['sometimes', 'email', 'max:255', Rule::unique('users', 'email')->ignore($user->id)],
-            'password' => ['sometimes', 'string', 'min:6', 'confirmed'],
-        ]);
+        $validated = $request->validated();
 
         $user->update($validated);
 
