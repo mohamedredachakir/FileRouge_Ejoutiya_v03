267187d2f664f8a4f7bd70615721e2109fb9f71a
reddcs <chakirm082@gmail.com>
2026-04-18 14:28:38 +0100

    chore: update frontend/src/components/layout/RouteTransitionShell.vue

---
+++ frontend/src/components/layout/RouteTransitionShell.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import { computed } from 'vue'
+const route = useRoute()
+const transition = computed(() => route.meta.transition || 'page-fade')
+</script>
+<template>
+  <RouterView v-slot="{ Component }">
+    <Transition :name="transition" mode="out-in">
+      <component :is="Component" :key="route.fullPath" />
+    </Transition>
+  </RouterView>
+</template>
