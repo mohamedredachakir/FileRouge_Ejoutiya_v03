72e972b131ceeb545f1dd8766559692f4dad27c4
reddcs <chakirm082@gmail.com>
2026-04-18 14:28:38 +0100

    chore: update frontend/src/components/base/BaseModal.vue

---
+++ frontend/src/components/base/BaseModal.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+defineProps<{ show: boolean }>()
+const emit = defineEmits(['close'])
+function onOverlayClick(e: MouseEvent) {
+  if ((e.target as HTMLElement).classList.contains('auth-overlay')) emit('close')
+}
+</script>
+<template>
+  <Teleport to="body">
+    <Transition name="modal">
+      <div v-if="show" class="auth-overlay" @click="onOverlayClick">
+        <div class="auth-box">
+          <slot />
+        </div>
+      </div>
+    </Transition>
+  </Teleport>
+</template>
